fa3eb5648c7edf6b817b975abea48610b7e1baf5
archisf <arkadyaristov@gmail.com>
2022-02-03 15:15:48 +0200

    first page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
 import { Route, Switch } from 'react-router';
-import { App as TodoApp } from 'app/containers/App';
+
+import React from 'react';
+// import { App as TodoApp } from 'app/containers/App';
 import { hot } from 'react-hot-loader';
+
+const FirstPage = () => (
+  <div>
+    <h1>Arkadii Aristov</h1>
+    <p>Hey you, thanks for coming. This is my personal page and feels free to contact me.</p>
+    <a href='mailto:arkadyaristov@gmail.com'>arkadyaristov@gmail.com</a>
+  </div>)
 
 export const App = hot(module)(() => (
   <Switch>
-    <Route path="/" component={TodoApp} />
+    <Route path="/" component={FirstPage} />
   </Switch>
 ));
